34565a204bb5bd88f7ce8616d8f42103825b6303
monsterkodi <monsterkodi@gmx.net>
2018-06-01 23:32:46 +0200

    build

--- bin/buildwin.sh
+++ bin/buildwin.sh
@@ -8,4 +8,5 @@ node_modules/.bin/electron-rebuild
 IGNORE="(.*\.dmg$|Icon$|coffee$|.*md$|/inno$|/svg$|/pug$|/styl$|.*\.noon$|.*\.lock$|bin$|kali.png|browser.png|shot.*\.png|.*\.bmp|.*\.icns)"
 node_modules/electron-packager/cli.js . --overwrite --icon=img/kaligraf.ico --ignore $IGNORE
 
-cp -f ./coffee/menu.noon kaligraf-win32-x64/resources/app/js
+mkdir kaligraf-win32-x64/resources/app/coffee
+cp -f ./coffee/menu.noon kaligraf-win32-x64/resources/app/coffee
